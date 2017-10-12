31a7efcede8a8d728ad4752a3e07e6f314255052
Jaks <jaks@localhost.localdomain>
2017-10-12 11:37:52 +0200

    started multithreading - not compiling

--- ex3.c
+++ ex3.c
@@ -1,7 +1,15 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <pthread.h>
 
+
+typedef struct args_struct
+{
+	int* v;
+	int vsize;
+	int value;
+} args_struct;
 
 int *initVector(char* sourcefile, int* size)
 {
@@ -42,7 +50,7 @@ int *initVector(char* sourcefile, int* size)
 
 }
 
-int search(int* vect, int n, int x)
+void search(int* vect, int n, int x)
 {
 	int i=0;
 	while ((i < n) && vect[i] != x)
@@ -50,7 +58,12 @@ int search(int* vect, int n, int x)
 		i++;
 	}
 
-	return vect[i]==x?1:0;
+	if (pthread_self() == t1)
+		while (pthread_kill(t2, SIGKILL) != 0);
+	else
+		while (pthread_kill(t1, SIGKILL) != 0);
+
+	vect[i]==x?printf("Value exists !"):printf("Value does not exist!");
 }
 
 
@@ -65,14 +78,46 @@ int main(int argc, char** argv)
 	int* vector;
 	int vector_size;
 	int val; 
+	pthread_t t1, t2;
 
 	vector = initVector(argv[1], &vector_size);
 
 	printf("Search for number : ");
 	scanf("%d", &val);
 
-	//printf("taille : %lu", sizeof(vector));
-	(search(vector, vector_size, val) == 1)?printf("Valeur présente"):printf("Valeur absente");
+
+	//singlethreaded version
+	//(search(vector, vector_size, val) == 1)?printf("Valeur présente"):printf("Valeur absente");
+
+
+	//multithreaded version
+
+	int* vector1 = malloc((vector_size/2)*sizeof(int));
+	int* vector2 = malloc((vector_size-(vector_size/2))*sizeof(int));
+	int i;
+	for (i = 0; i < vector_size; i++)
+	{
+		if (i <= vector_size/2)
+			vector1[i] = vector[i];
+		else
+			vector2[i-vector_size/2] = vector[i];
+	}
+
+	// first half of vector
+	args_struct args_t1;
+	args->v = vector1;
+	args->vsize = vector_size/2;
+	args->value = val;
+
+	// second half of vector
+	args_struct args_t2;
+	args->v = vector2;
+	args->vsize = vector_size-(vector_size/2);
+	args->value = val;
+
+	pthread_create(&t1, NULL, search, args_t1);
+	pthread_create(&t2, NULL, search, args_t2);
+
 
 	return 0;
 
